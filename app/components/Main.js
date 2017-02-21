6282803767a38b8835f399d54e733fcb9ff805ba
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2017-02-20 23:31:03 -0800

    Added app, model, public folder, created article and note schema, and set up component file structure

---
+++ app/components/Main.js
@@ -0,0 +1,25 @@
+var React = require("react");
+
+//Children
+var Saved = require("./Children/Saved.js");
+var Search = require("./Children/Search.js");
+
+//utils in the future?
+// var helper = require("./utils/helper.js");
+
+var Main = React.createClass({
+    getInitialState: function(){
+      return ()
+    },
+    //Do this right after the page renders
+    componentDidMount: function(){
+      //Load Saved Articles
+    },
+    //Do this after component changes
+    componentDidUpdate: function(){
+
+    }
+
+});
+
+module.exports = Main;
---
+++ models/Article.js
@@ -0,0 +1,31 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }]
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
